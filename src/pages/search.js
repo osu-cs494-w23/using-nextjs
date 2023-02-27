a063ff706cf3c13ff7808f874d3c21de3ec6810a
Rob Hess <hessro@oregonstate.edu>
2023-02-27 10:04:55 -0800

    Set up code for lecture.

---
+++ src/pages/search.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+/*
+ * This Search component always executes an API call on the client side, using
+ * the useEffect() hook.
+ */
+export default function Search() {
+  const router = useRouter();
+  const query = router.query.q;
+  const [ inputQuery, setInputQuery ] = useState(query || "");
+  const [ repos, setRepos ] = useState([]);
+
+  useEffect(() => {
+    if (query) {
+      async function fetchSearchResults() {
+        console.log("== Fetching search results for query:", query);
+        setRepos(null);
+        const response = await fetch(
+          `https://api.github.com/search/repositories?q=${query}&sort=stars`
+        );
+        const responseBody = await response.json();
+        setRepos(responseBody.items || []);
+      }
+
+      fetchSearchResults();
+    }
+  }, [ query ]);
+
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        router.push(`${router.pathname}?q=${inputQuery}`);
+      }}>
+        <input
+          value={inputQuery}
+          onChange={e => setInputQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {!repos && <p>Loading...</p> }
+      {repos && (
+        <ul>
+          {repos.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.html_url}>{repo.full_name}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
